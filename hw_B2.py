7da25eaa6d6382541ad98cae7831d90743f0a314
Viktoria Gavrilova <48158005+vicoso@users.noreply.github.com>
2019-03-25 10:07:28 +0300

    Update hw_B2.py

--- hw_B2.py
+++ hw_B2.py
@@ -1,6 +1,3 @@
 a=input(' ')
-b=(a.replace('T', 'H'), a.replace('C','P'))
-c=(b.replace('A', 'T'), b.replace('G','C'))
-f=(c.replace('H', 'A'), c.replace('P','G'))
-reversed_f=f[::-1]
-print(reversed_f)
+a=(a.replace('T', 'a').replace('C','g').replace('G', 'c').replace('A', 't').upper()[::-1])
+print(a)
