fe410774a2b1d8083da9ea45b786bf800e452a4e
Viktoria Gavrilova <48158005+vicoso@users.noreply.github.com>
2019-03-25 11:16:41 +0300

    Update hw_B2.py

--- hw_B2.py
+++ hw_B2.py
@@ -1,3 +1,11 @@
+1st variant
+
 a=input(' ')
 a=(a.replace('T', 'a').replace('C','g').replace('G', 'c').replace('A', 't').upper()[::-1])
 print(a)
+
+
+2nd variant
+
+s = input(' ') 
+print(s[::-1].translate(str.maketrans('ACGT', 'TGCA')))
