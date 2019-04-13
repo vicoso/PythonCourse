9c8f9aec86dbaa07412a332fa86eb9e27680373e
Viktoria Gavrilova <48158005+vicoso@users.noreply.github.com>
2019-04-13 10:14:57 +0300

    Add files via upload

---
+++ FIB.py
@@ -0,0 +1,11 @@
+n = int(input())
+k = int(input())
+def fib(n):
+    if n in (1, 2):
+            return 1
+    else:
+        n<=40
+        k<=5
+        return fib(n-1) + k*fib(n-2)
+print(fib(n))      
+    
