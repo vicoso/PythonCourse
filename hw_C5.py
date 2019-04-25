f7bc050e263f6d3e951e452f121f60c08e942f02
Viktoria Gavrilova <48158005+vicoso@users.noreply.github.com>
2019-04-25 18:31:53 +0300

    Update hw_C5.py

--- hw_C5.py
+++ hw_C5.py
@@ -4,8 +4,8 @@ def fib(n):
     if n in (1, 2):
             return 1
     else:
-        n<=40
-        k<=5
+        #n<=40
+        #k<=5
         return fib(n-1) + k*fib(n-2)
 print(fib(n))      
     
