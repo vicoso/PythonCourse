e61bc36e333196daa822d48af9038c84e763a316
Viktoria Gavrilova <48158005+vicoso@users.noreply.github.com>
2019-04-27 01:37:18 +0300

    Update hw_E1.py

--- hw_E1.py
+++ hw_E1.py
@@ -1,3 +1,4 @@
+#есть такой варинат
 import matplotlib.pyplot as plt
 import numpy as np
 n=np.random.poisson(10000)
@@ -6,3 +7,14 @@ axes.hist(n)
 axes.set_title('Poisson dstrb')
 axes.set_xlim((min(str((n), max(str(n)))
 plt.show()
+                   
+#и такой вариант, но они оба какие-то полурабочие у меня
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import *
+n=np.random.poisson(10, 10000)
+fig, axes=plt.subplots(1, 2, figsize=(12,4))
+
+axes[0].hist(n)
+axes[0].set_title('poisson')
+axes[0].set_xlim((min(n), max(n)))
